1bb150d44e2d287591a96d6949004b3be99fdea7
Daniel Mason <daniel@m2.nz>
2023-04-22 12:37:30 +1200

    Initial Commit

---
+++ data.tf
@@ -0,0 +1,23 @@
+# Get current AWS Region
+data "aws_region" "current" {}
+
+# Get available AZs
+data "aws_availability_zones" "available" {}
+
+# Get latest official Ubuntu AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-arm64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
